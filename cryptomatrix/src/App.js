fb6c439a759463ae73ddcb6333d55dc5a7cd8a37
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-07-03 23:01:58 +0530

    Home page complete

--- cryptomatrix/src/App.js
+++ cryptomatrix/src/App.js
@@ -35,6 +35,9 @@ function App() {
           ]}
           className="dropdown"
         ></Select>
+        <Routes>
+          <Route path="/" element={<Home/>}/>;
+        </Routes>
       </header>
     </div>
   );
